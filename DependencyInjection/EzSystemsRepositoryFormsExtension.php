33aa20b2bfe65df26cf53b8559bbad1c87895c25
Jérôme Vieilledent <lolautruche@gmail.com>
2015-03-05 16:23:14 +0100

    First commit

---
+++ DependencyInjection/EzSystemsRepositoryFormsExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryFormsBundle package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryFormsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class EzSystemsRepositoryFormsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ EzSystemsRepositoryFormsBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryFormsBundle package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryFormsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsRepositoryFormsBundle extends Bundle
+{
+}
