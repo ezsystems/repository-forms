d2de9989a97425309ddb7687fdf6152b7d127e62
Jérôme Vieilledent <lolautruche@gmail.com>
2015-04-23 09:58:21 +0200

    EZP-24253: Implemented form types for ContentType update and FieldDefinition update

---
+++ Form/Type/ContentTypeUpdateType.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryFormsBundle\Form\Type;
+
+use eZ\Publish\API\Repository\Values\Content\Location;
+use EzSystems\RepositoryFormsBundle\Form\DataTransformer\TranslatablePropertyTransformer;
+use EzSystems\RepositoryFormsBundle\Form\Type\FieldDefinition\FieldDefinitionType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Form type for ContentType update.
+ *
+ * @author Jérôme Vieilledent <jerome.vieilledent@ez.no>
+ */
+class ContentTypeUpdateType extends AbstractType
+{
+    public function getName()
+    {
+        return 'ezrepoforms_contenttype_update';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'data_class' => 'EzSystems\RepositoryFormsBundle\Data\ContentTypeData'
+            ])
+            ->setRequired(['languageCode']);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $translatablePropertyTransformer = new TranslatablePropertyTransformer($options['languageCode']);
+        $builder
+            ->add(
+                $builder
+                    ->create('name', 'text', ['property_path' => 'names'])
+                    ->addModelTransformer($translatablePropertyTransformer)
+            )
+            ->add('identifier', 'text')
+            ->add(
+                $builder
+                    ->create('description', 'text', ['property_path' => 'descriptions', 'required' => false])
+                    ->addModelTransformer($translatablePropertyTransformer)
+            )
+            ->add('nameSchema', 'text', ['required' => false])
+            ->add('urlAliasSchema', 'text', ['required' => false])
+            ->add('isContainer', 'checkbox', ['required' => false])
+            ->add('defaultSortField', 'choice', [
+                'choices' => [
+                    Location::SORT_FIELD_NAME => 'Content name',
+                    Location::SORT_FIELD_CLASS_NAME => 'ContentType name',
+                    Location::SORT_FIELD_CLASS_IDENTIFIER => 'ContentType identifier',
+                    Location::SORT_FIELD_DEPTH => 'Location depth',
+                    Location::SORT_FIELD_PATH => 'Location path',
+                    Location::SORT_FIELD_PRIORITY => 'Location priority',
+                    Location::SORT_FIELD_MODIFIED => 'Modification date',
+                    Location::SORT_FIELD_PUBLISHED => 'Publication date',
+                    Location::SORT_FIELD_SECTION => 'Section',
+                ]
+            ])
+            ->add('defaultSortOrder', 'choice', [
+                'choices' => [
+                    Location::SORT_ORDER_ASC => 'Ascending',
+                    Location::SORT_ORDER_DESC => 'Descending',
+                ]
+            ])
+            ->add('defaultAlwaysAvailable', 'checkbox', ['required' => false])
+            ->add('fieldDefinitionsData', 'collection', [
+                'type' => new FieldDefinitionType(),
+                'options' => ['languageCode' => $options['languageCode']]
+            ])
+            ->add('fieldTypeSelection', 'choice', [
+                'choices' => ['ezstring' => 'Text line'],
+                'mapped' => false
+            ])
+            ->add('addFieldDefinition', 'submit', ['label' => 'Add field definition'])
+            ->add('saveContentType', 'submit', ['label' => 'Update']);
+    }
+}
---
+++ Form/Type/FieldDefinition/FieldDefinitionType.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryFormsBundle\Form\Type\FieldDefinition;
+
+use EzSystems\RepositoryFormsBundle\Form\DataTransformer\TranslatablePropertyTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Form type for FieldDefinition update.
+ *
+ * @author Jérôme Vieilledent <jerome.vieilledent@ez.no>
+ */
+class FieldDefinitionType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'data_class' => 'EzSystems\RepositoryFormsBundle\Data\FieldDefinitionData'
+            ])
+            ->setRequired(['languageCode']);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $translatablePropertyTransformer = new TranslatablePropertyTransformer($options['languageCode']);
+        $builder
+            ->add('fieldTypeIdentifier', 'hidden')
+            ->add(
+                $builder->create('name', 'text', ['property_path' => 'names'])
+                    ->addModelTransformer($translatablePropertyTransformer)
+            )
+            ->add('identifier', 'text', ['required' => true])
+            ->add(
+                $builder->create('description', 'text', ['property_path' => 'descriptions', 'required' => false])
+                    ->addModelTransformer($translatablePropertyTransformer)
+            )
+            ->add('isRequired', 'checkbox', ['required' => false])
+            ->add('isSearchable', 'checkbox', ['required' => false])
+            ->add('isTranslatable', 'checkbox', ['required' => false])
+            ->add('fieldGroup', 'choice', ['choices' => []], ['required' => false])
+            ->add('position', 'integer');
+
+        // Hook on form generation for specific FieldType needs
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            /** @var \EzSystems\RepositoryFormsBundle\Data\FieldDefinitionData $data */
+            $data = $event->getData();
+            // TODO: To be refactored cleanly with a registry and an interface.
+            // FieldTypes may define a service that would alter the form for their own needs.
+            switch ($data->getFieldTypeIdentifier()) {
+                case 'ezstring':
+                    $form
+                        ->add('minLength', 'integer', [
+                            'required' => false,
+                            'property_path' => 'validatorConfiguration[StringLengthValidator][minStringLength]',
+                        ])
+                        ->add('maxLength', 'integer', [
+                            'required' => false,
+                            'property_path' => 'validatorConfiguration[StringLengthValidator][maxStringLength]',
+                        ]);
+                    break;
+            }
+        });
+    }
+
+    public function getName()
+    {
+        return 'ezrepoforms_fielddefinition_update';
+    }
+}
