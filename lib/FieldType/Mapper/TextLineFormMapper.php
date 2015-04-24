ae39d0b3b3d590ddc48aecc84617850b26acf8aa
Jérôme Vieilledent <lolautruche@gmail.com>
2015-04-24 16:17:47 +0200

    EZP-24306: Implement FieldDefinition form mapper for ezstring

---
+++ lib/FieldType/Mapper/TextLineFormMapper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\FieldType\Mapper;;
+
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use EzSystems\RepositoryForms\FieldType\FieldTypeFormMapperInterface;
+use Symfony\Component\Form\FormInterface;
+
+class TextLineFormMapper implements FieldTypeFormMapperInterface
+{
+    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data)
+    {
+        $fieldDefinitionForm
+            ->add('minLength', 'integer', [
+                'required' => false,
+                'property_path' => 'validatorConfiguration[StringLengthValidator][minStringLength]',
+            ])
+            ->add('maxLength', 'integer', [
+                'required' => false,
+                'property_path' => 'validatorConfiguration[StringLengthValidator][maxStringLength]',
+            ]);
+    }
+}
