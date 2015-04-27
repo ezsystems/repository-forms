1e6aaad4441e7d3b47b39af0eea87c266bbaf9c8
Jérôme Vieilledent <lolautruche@gmail.com>
2015-04-27 14:55:31 +0200

    EZP-24303: Added tests for ContentTypeData and FieldDefinitionData

---
+++ tests/RepositoryForms/Data/ContentTypeDataTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\Tests\Data;
+
+use EzSystems\RepositoryForms\Data\ContentTypeData;
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use PHPUnit_Framework_TestCase;
+
+class ContentTypeDataTest extends PHPUnit_Framework_TestCase
+{
+    public function testContentTypeDraft()
+    {
+        $contentTypeDraft = $this->getMockForAbstractClass('\eZ\Publish\API\Repository\Values\ContentType\ContentTypeDraft');
+        $data = new ContentTypeData(['contentTypeDraft' => $contentTypeDraft]);
+        self::assertSame($contentTypeDraft, $data->contentTypeDraft);
+    }
+
+    public function testFieldDefinitionData()
+    {
+        $fieldDef1 = new FieldDefinitionData();
+        $fieldDef2 = new FieldDefinitionData();
+        $fieldDef3 = new FieldDefinitionData();
+        $fieldDef4 = new FieldDefinitionData();
+
+        $initialFieldDefs = [$fieldDef1, $fieldDef2];
+        $data = new ContentTypeData(['fieldDefinitionsData' => $initialFieldDefs]);
+        self::assertSame($initialFieldDefs, $data->fieldDefinitionsData);
+
+        $data->addFieldDefinitionData($fieldDef3);
+        $data->addFieldDefinitionData($fieldDef4);
+        self::assertSame([$fieldDef1, $fieldDef2, $fieldDef3, $fieldDef4], $data->fieldDefinitionsData);
+    }
+}
---
+++ tests/RepositoryForms/Data/FieldDefinitionDataTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\Tests\RepositoryForms\Data;
+
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use PHPUnit_Framework_TestCase;
+
+class FieldDefinitionDataTest extends PHPUnit_Framework_TestCase
+{
+    public function testFieldDefinition()
+    {
+        $fieldDefinition = $this->getMockForAbstractClass('\eZ\Publish\API\Repository\Values\ContentType\FieldDefinition');
+        $data = new FieldDefinitionData(['fieldDefinition' => $fieldDefinition]);
+        self::assertSame($fieldDefinition, $data->fieldDefinition);
+    }
+
+    public function testGetFieldTypeIdentifier()
+    {
+        $fieldTypeIdentifier = 'ezstring';
+        $fieldDefinition = $this->getMockBuilder('\eZ\Publish\API\Repository\Values\ContentType\FieldDefinition')
+            ->setConstructorArgs([['fieldTypeIdentifier' => $fieldTypeIdentifier]])
+            ->getMockForAbstractClass();
+        $data = new FieldDefinitionData(['fieldDefinition' => $fieldDefinition]);
+        self::assertSame($fieldTypeIdentifier, $data->getFieldTypeIdentifier());
+    }
+}
