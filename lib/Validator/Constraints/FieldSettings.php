fd8da4e98a284943c6c451983cd630b43f2a10a3
Jérôme Vieilledent <lolautruche@gmail.com>
2015-05-07 17:54:57 +0200

    EZP-24309: Implemented FieldSettingsValidator constraint

---
+++ lib/Validator/Constraints/FieldSettings.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class FieldSettings extends Constraint
+{
+    public $message = 'ez.field_definition.field_settings';
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+
+    public function validatedBy()
+    {
+        return 'ezrepoforms.validator.field_settings';
+    }
+}
---
+++ lib/Validator/Constraints/FieldSettingsValidator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\Validator\Constraints;
+
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Will check if field settings for FieldDefinition are valid.
+ */
+class FieldSettingsValidator extends FieldTypeValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$value instanceof FieldDefinitionData) {
+            return;
+        }
+
+        $fieldType = $this->fieldTypeService->getFieldType($value->getFieldTypeIdentifier());
+        $this->processValidationErrors($fieldType->validateFieldSettings($value->fieldSettings));
+    }
+}
