b84aafae6d402b92e297290caa3d05fe614ecfb2
Jérôme Vieilledent <lolautruche@gmail.com>
2015-05-07 17:54:39 +0200

    EZP-24309: Implemented ValidatorConfigurationValidator constraint

---
+++ lib/Validator/Constraints/ValidatorConfigurationValidator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\Validator\Constraints;
+
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Will check if validator configuration for FieldDefinition is valid.
+ */
+class ValidatorConfigurationValidator extends FieldTypeValidator
+{
+    /**
+     * Checks if the passed value is valid.
+     *
+     * @param FieldDefinitionData $value The value that should be validated
+     * @param Constraint $constraint The constraint for the validation
+     *
+     * @api
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$value instanceof FieldDefinitionData) {
+            return;
+        }
+
+        $fieldType = $this->fieldTypeService->getFieldType($value->getFieldTypeIdentifier());
+        $this->processValidationErrors($fieldType->validateValidatorConfiguration($value->validatorConfiguration));
+    }
+}
