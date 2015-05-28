fd167de421a5a0c0e459e186d0d9b69bbcaf2d63
Gunnstein Lye <gunnstein.lye@ez.no>
2015-05-28 13:36:47 +0200

    Fix EZP-24398: Implement FieldDefinition form mapper for ezisbn

---
+++ lib/FieldType/Mapper/ISBNFormMapper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\FieldType\Mapper;
+
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use EzSystems\RepositoryForms\FieldType\FieldTypeFormMapperInterface;
+use Symfony\Component\Form\FormInterface;
+
+class ISBNFormMapper implements FieldTypeFormMapperInterface
+{
+    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data)
+    {
+        $fieldDefinitionForm
+            ->add(
+                'isISBN13', 'checkbox', [
+                    'required' => false,
+                    'property_path' => 'fieldSettings[isISBN13]',
+                    'label' => 'field_definition.ezisbn.is_isbn13',
+                ]
+            );
+    }
+}
