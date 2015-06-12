bd59e3e4b57adc79e75994af2317bdf4a425a786
Gunnstein Lye <gunnstein.lye@ez.no>
2015-06-12 14:45:09 +0200

    Fix EZP-24446: Implement ezrelation FieldDefinition form mapper
    Using UDW button for object selection, this not testable yet
    Removed selectionMethod property, not relevant anymore
    Using new viewType for showing name of selected root node

---
+++ lib/FieldType/Mapper/RelationFormMapper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RepositoryForms\FieldType\Mapper;
+
+use eZ\Publish\Core\FieldType\Relation\Type;
+use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use EzSystems\RepositoryForms\FieldType\FieldTypeFormMapperInterface;
+use Symfony\Component\Form\FormInterface;
+
+class RelationFormMapper implements FieldTypeFormMapperInterface
+{
+    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data)
+    {
+        $fieldDefinitionForm
+            ->add('selectionRoot', 'text', [ // TODO: Replace 'text' with 'hidden' when the UDW is ready to use
+                'required' => false,
+                'property_path' => 'fieldSettings[selectionRoot]',
+                'label' => 'field_definition.ezobjectrelation.selection_root',
+            ]);
+    }
+}
