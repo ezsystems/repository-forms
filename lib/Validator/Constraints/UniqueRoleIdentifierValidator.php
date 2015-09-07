eef4256356f05ae047968d520a6143ab6fb664cc
Jérôme Vieilledent <lolautruche@gmail.com>
2015-09-07 17:33:53 +0200

    Added validation to Role editing

---
+++ lib/Validator/Constraints/UniqueRoleIdentifierValidator.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryForms\Validator\Constraints;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\RoleService;
+use EzSystems\RepositoryForms\Data\RoleData;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueRoleIdentifierValidator extends ConstraintValidator
+{
+    /**
+     * @var RoleService
+     */
+    private $roleService;
+
+    public function __construct(RoleService $roleService)
+    {
+        $this->roleService = $roleService;
+    }
+
+    /**
+     * Checks if the passed value is valid.
+     *
+     * @param mixed $value The value that should be validated
+     * @param Constraint $constraint The constraint for the validation
+     *
+     * @api
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$value instanceof RoleData) {
+            return;
+        }
+
+        try {
+            $role = $this->roleService->loadRoleByIdentifier($value->identifier);
+            // It's of course OK to edit a draft of an existing Role :-)
+            if ($role->id === $value->role->id) {
+                return;
+            }
+
+            $this->context->buildViolation($constraint->message)
+                ->atPath('identifier')
+                ->setParameter('%identifier%', $value->identifier)
+                ->addViolation();
+        } catch (NotFoundException $e) {
+            // Do nothing
+        }
+    }
+}
