ab9b8c16f0131537b29e6b6efbe9b91dc105ee71
Jérôme Vieilledent <lolautruche@gmail.com>
2015-11-16 17:11:30 +0100

    EZP-25100: Added rough ContentEditController

---
+++ bundle/Controller/ContentEditController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryFormsBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use EzSystems\RepositoryForms\Data\Content\ContentCreateData;
+use EzSystems\RepositoryForms\Data\Content\FieldData;
+
+class ContentEditController extends Controller
+{
+    public function createWithoutDraftAction($contentTypeId, $language, $parentLocationId)
+    {
+        $contentTypeService = $this->getRepository()->getContentTypeService();
+        $contentType = $contentTypeService->loadContentType($contentTypeId);
+        $data = new ContentCreateData(['contentTypeId' => $contentTypeId, 'mainLanguageCode' => $language]);
+        foreach ($contentType->fieldDefinitions as $fieldDef) {
+            $data->addFieldData(new FieldData(['fieldDefinition' => $fieldDef]));
+        }
+    }
+
+    public function createAction($contentTypeId, $language, $parentLocationId)
+    {
+
+    }
+
+    public function editAction($contentId, $version, $language = null)
+    {
+
+    }
+}
