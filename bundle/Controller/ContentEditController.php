6c607097d5c46119b830bc36aa053201299b10fd
Jérôme Vieilledent <lolautruche@gmail.com>
2015-11-18 17:08:28 +0100

    EZP-25100: Added support for editing a new content version

--- bundle/Controller/ContentEditController.php
+++ bundle/Controller/ContentEditController.php
@@ -9,9 +9,11 @@
 namespace EzSystems\RepositoryFormsBundle\Controller;
 
 use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\ContentService;
 use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\LocationService;
 use EzSystems\RepositoryForms\Data\Mapper\ContentCreateMapper;
+use EzSystems\RepositoryForms\Data\Mapper\ContentUpdateMapper;
 use EzSystems\RepositoryForms\Form\ActionDispatcher\ActionDispatcherInterface;
 use EzSystems\RepositoryForms\Form\Type\Content\ContentEditType;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +24,11 @@ class ContentEditController extends Controller
      * @var ContentTypeService
      */
     private $contentTypeService;
+
+    /**
+     * @var ContentService
+     */
+    private $contentService;
 
     /**
      * @var LocationService
@@ -35,12 +42,14 @@ class ContentEditController extends Controller
 
     public function __construct(
         ContentTypeService $contentTypeService,
+        ContentService $contentService,
         LocationService $locationService,
         ActionDispatcherInterface $contentActionDispatcher
     ) {
         $this->contentTypeService = $contentTypeService;
         $this->locationService = $locationService;
         $this->contentActionDispatcher = $contentActionDispatcher;
+        $this->contentService = $contentService;
     }
 
     public function createWithoutDraftAction($contentTypeId, $language, $parentLocationId, Request $request)
@@ -69,7 +78,35 @@ class ContentEditController extends Controller
     {
     }
 
-    public function editAction($contentId, $version, $language = null)
+    public function editAction($contentId, $version, $language, Request $request)
     {
+        // Create a new version if none is provided explicitly
+        if ($version == 0) {
+            $contentDraft = $this->contentService->createContentDraft($this->contentService->loadContentInfo($contentId));
+        } else {
+            $contentDraft = $this->contentService->loadContent($contentId, $language ? [$language] : null, $version);
+        }
+
+        $contentType = $this->contentTypeService->loadContentType($contentDraft->contentInfo->contentTypeId);
+        $data = (new ContentUpdateMapper())->mapToFormData($contentDraft, [
+            'languageCode' => $language,
+            'contentType' => $contentType,
+        ]);
+        $form = $this->createForm(new ContentEditType(), $data, [
+            'languageCode' => $language,
+            'drafts_enabled' => true,
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->contentActionDispatcher->dispatchFormAction($form, $data, $form->getClickedButton()->getName());
+            if ($response = $this->contentActionDispatcher->getResponse()) {
+                return $response;
+            }
+        }
+
+        return $this->render('EzSystemsRepositoryFormsBundle:Content:content_create_no_draft.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
