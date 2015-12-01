bd038fe7052a20af45bf3355276c49dc99600fd4
Bertrand Dunogier <bd@ez.no>
2015-12-01 15:44:04 +0100

    fixup! EZP-25100: prototyped behat coverage of content edit

--- features/ContentEdit/create_without_draft.feature
+++ features/ContentEdit/create_without_draft.feature
@@ -4,8 +4,11 @@ Feature: Edit content
     I need to expose a form that creates a content item without using a draft.
 
 Scenario: Create a folder without a draft
-    Given there is a Content Type "folder" with the id "1"
+    Given that I have permission to create folders
+      And there is a Content Type "folder" with the id "1"
       #And there is a Location with the id "2"
      When I go to "content/create/nodraft/1/eng-GB/2"
-     Then I should see a content edit form
-
+     Then I should see a folder content edit form
+     When I fill in the folder edit form
+      And I press "Publish"
+     Then I am on the View of the Content that was published
--- features/Context/ContentEdit.php
+++ features/Context/ContentEdit.php
@@ -6,16 +6,57 @@ namespace EzSystems\RepositoryForms\Features\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\MinkExtension\Context\MinkContext;
+use PHPUnit_Framework_Assert as Assertion;
 
 class ContentEdit extends MinkContext implements Context, SnippetAcceptingContext
 {
     /**
+     * The
+     */
+    private $createdContentName;
+
+    /**
+     * @Then /^I should see a folder content edit form$/
      * @Then /^I should see a content edit form$/
      */
     public function iShouldSeeAContentEditForm()
     {
         $this->assertSession()->elementExists('css', 'form[name=ezrepoforms_content_edit]');
+    }
+
+    /**
+     * @Then /^I am on the View of the Content that was published$/
+     */
+    public function iAmOnTheViewOfTheContentThatWasPublished()
+    {
+        if (!isset($this->createdContentName)) {
+            throw new \Exception("No created content name set");
+        }
+
+        $page = $this->getSession()->getPage();
+        Assertion::assertTrue($page->has('css', 'span.ezstring-field'));
+        Assertion::assertEquals($this->createdContentName, $page->find('css', 'span.ezstring-field')->getText());
+    }
+
+    /**
+     * @When /^I fill in the folder edit form$/
+     */
+    public function iFillInTheFolderEditForm()
+    {
+        // will only work for single value fields
+        $this->createdContentName = "Behat content edit @" . microtime(true);
+        $this->fillField("ezrepoforms_content_edit_fieldsData_name_value", $this->createdContentName);
+    }
+
+    /**
+     * @Given /^that I have permission to create folders$/
+     */
+    public function thatIHavePermissionToCreateFolders()
+    {
+        $this->visit('/login');
+        $this->fillField('Username', 'admin');
+        $this->fillField('Password', 'publish');
+        $this->pressButton('Login');
     }
 }
