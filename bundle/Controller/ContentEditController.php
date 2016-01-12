76f38ac57a7f79e3f697a2f9a60c0c70da892945
Jérôme Vieilledent <lolautruche@gmail.com>
2016-01-12 13:14:41 +0100

    EZP-25100: Content edit controller
    Implements the createWithoutDraft action + route.

---
+++ bundle/Controller/ContentEditController.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryFormsBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\LocationService;
+use EzSystems\RepositoryForms\Data\Mapper\ContentCreateMapper;
+use EzSystems\RepositoryForms\Form\ActionDispatcher\ActionDispatcherInterface;
+use EzSystems\RepositoryForms\Form\Type\Content\ContentEditType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContentEditController extends Controller
+{
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var LocationService
+     */
+    private $locationService;
+
+    /**
+     * @var ActionDispatcherInterface
+     */
+    private $contentActionDispatcher;
+
+    public function __construct(
+        ContentTypeService $contentTypeService,
+        ContentService $contentService,
+        LocationService $locationService,
+        ActionDispatcherInterface $contentActionDispatcher
+    ) {
+        $this->contentTypeService = $contentTypeService;
+        $this->locationService = $locationService;
+        $this->contentActionDispatcher = $contentActionDispatcher;
+        $this->contentService = $contentService;
+    }
+
+    /**
+     * Displays and processes a content creation form. Showing the form does not create a draft in the repository.
+     *
+     * @param int $contentTypeIdentifier ContentType id to create
+     * @param string $language Language code to create the content in (eng-GB, ger-DE, ...))
+     * @param int $parentLocationId Location the content should be a child of
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createWithoutDraftAction($contentTypeIdentifier, $language, $parentLocationId, Request $request)
+    {
+        $contentType = $this->contentTypeService->loadContentTypeByIdentifier($contentTypeIdentifier);
+        $data = (new ContentCreateMapper())->mapToFormData($contentType, [
+            'mainLanguageCode' => $language,
+            'parentLocation' => $this->locationService->newLocationCreateStruct($parentLocationId),
+        ]);
+        $form = $this->createForm(new ContentEditType(), $data, ['languageCode' => $language]);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->contentActionDispatcher->dispatchFormAction($form, $data, $form->getClickedButton()->getName());
+            if ($response = $this->contentActionDispatcher->getResponse()) {
+                return $response;
+            }
+        }
+
+        return $this->render('EzSystemsRepositoryFormsBundle:Content:content_edit.html.twig', [
+            'form' => $form->createView(),
+            'languageCode' => $language,
+        ]);
+    }
+}
