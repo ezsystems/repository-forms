ded8d0cd2da92dcf75878396fc8a7ebd2e633aae
Bertrand Dunogier <bertrand.dunogier@ez.no>
2016-05-20 16:56:36 +0200

    EZP-25730: simple registration support

---
+++ bundle/Controller/UserRegisterController.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryFormsBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\UserService;
+use EzSystems\RepositoryForms\Data\Mapper\UserRegisterMapper;
+use EzSystems\RepositoryForms\Form\ActionDispatcher\ActionDispatcherInterface;
+use EzSystems\RepositoryForms\Form\Type\User\UserRegisterType;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRegisterController extends Controller
+{
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * @var ActionDispatcherInterface
+     */
+    private $contentActionDispatcher;
+
+    /**
+     * @var Repository
+     */
+    private $repository;
+
+    /**
+     * @var string
+     */
+    private $pagelayout;
+
+    public function __construct(
+        ContentTypeService $contentTypeService,
+        UserService $userService,
+        ActionDispatcherInterface $contentActionDispatcher,
+        Repository $repository
+    ) {
+        $this->contentTypeService = $contentTypeService;
+        $this->userService = $userService;
+        $this->contentActionDispatcher = $contentActionDispatcher;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param string $pagelayout
+     * @return ContentEditController
+     */
+    public function setPagelayout($pagelayout)
+    {
+        $this->pagelayout = $pagelayout;
+
+        return $this;
+    }
+
+    /**
+     * Displays and processes a user registration form.
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $language = 'eng-GB';
+
+        $contentType = $this->repository->sudo(
+            function () {
+                return $this->contentTypeService->loadContentTypeByIdentifier('user');
+            }
+        );
+
+        $data = (new UserRegisterMapper())->mapToFormData($contentType, [
+            'mainLanguageCode' => $language,
+            'parentGroup' => $this->repository->sudo(
+                function () {
+                    return $this->userService->loadUserGroup(11);
+                }
+            ),
+        ]);
+        $form = $this->createForm(new UserRegisterType(), $data, ['languageCode' => $language]);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->contentActionDispatcher->dispatchFormAction($form, $data, $form->getClickedButton()->getName());
+            if ($response = $this->contentActionDispatcher->getResponse()) {
+                return $response;
+            }
+        }
+
+        return $this->render('EzSystemsRepositoryFormsBundle:Content:content_edit.html.twig', [
+            'form' => $form->createView(),
+            'languageCode' => $language,
+            'pagelayout' => $this->pagelayout,
+        ]);
+    }
+
+    public function registerConfirmAction()
+    {
+        return $this->render(
+            '@EzSystemsRepositoryForms/User/register_confirmation.html.twig',
+            ['pagelayout' => $this->pagelayout]
+        );
+    }
+}
---
+++ lib/Form/Processor/UserRegisterFormProcessor.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace EzSystems\RepositoryForms\Form\Processor;
+
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\UserService;
+use eZ\Publish\API\Repository\Values\Content\ContentStruct;
+use EzSystems\RepositoryForms\Data\User\UserCreateData;
+use EzSystems\RepositoryForms\Event\FormActionEvent;
+use EzSystems\RepositoryForms\Event\RepositoryFormEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Listens for and processes RepositoryForm events: publish, remove draft, save draft...
+ */
+class UserRegisterFormProcessor implements EventSubscriberInterface
+{
+    /**
+     * @var \eZ\Publish\API\Repository\UserService
+     */
+    private $userService;
+
+    /**
+     * @var \Symfony\Component\Routing\RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var Repository
+     */
+    private $repository;
+
+    public function __construct(Repository $repository, UserService $userService, RouterInterface $router)
+    {
+        $this->userService = $userService;
+        $this->router = $router;
+        $this->repository = $repository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            RepositoryFormEvents::CONTENT_PUBLISH => ['processPublish', 20],
+        ];
+    }
+
+    public function processPublish(FormActionEvent $event)
+    {
+        /** @var \EzSystems\RepositoryForms\Data\User\UserCreateData $data */
+        if (!($data = $event->getData()) instanceof UserCreateData) {
+            return;
+        }
+        $form = $event->getForm();
+
+        $this->saveDraft($data, $form->getConfig()->getOption('languageCode'));
+
+        $redirectUrl = $this->router->generate('ez_user_register_confirmation');
+        $event->setResponse(new RedirectResponse($redirectUrl));
+        $event->stopPropagation();
+    }
+
+    /**
+     * Saves content draft corresponding to $data.
+     * Depending on the nature of $data (create or update data), the draft will either be created or simply updated.
+     *
+     * @param ContentStruct|\EzSystems\RepositoryForms\Data\User\UserCreateData $data
+     * @param $languageCode
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     */
+    private function saveDraft(UserCreateData $data, $languageCode)
+    {
+        foreach ($data->fieldsData as $fieldDefIdentifier => $fieldData) {
+            if ($fieldData->getFieldTypeIdentifier() !== 'ezuser') {
+                $data->setField($fieldDefIdentifier, $fieldData->value, $languageCode);
+            }
+        }
+
+        return $this->repository->sudo(
+            function () use ($data) {
+                return $this->userService->createUser($data, $data->getParentGroups());
+            }
+        );
+    }
+}
