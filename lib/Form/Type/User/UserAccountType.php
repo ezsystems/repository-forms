37366279436aad38fd961bae9bca4f35e4210f69
Bertrand Dunogier <bertrand.dunogier@ez.no>
2016-05-20 16:56:36 +0200

    EZP-25730: user registration support

--- lib/Form/Type/User/UserAccountType.php
+++ lib/Form/Type/User/UserAccountType.php
@@ -6,13 +6,13 @@
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  */
-namespace EzSystems\RepositoryForms\Form\Type;
+namespace EzSystems\RepositoryForms\Form\Type\User;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class UserAccountType extends AbstractType
 {
     public function getName()
     {
@@ -23,14 +23,14 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username', 'text', [
-                'property_path' => 'login',
+                'property_path' => 'username',
                 'label' => 'content.field_type.ezuser.username',
                 'required' => $options['required'],
             ])
             ->add('password', 'repeated', [
                 'type' => 'password',
                 'required' => true,
-                'property_path' => 'passwordHash',
+                'property_path' => 'password',
                 'first_options' => ['label' => 'content.field_type.ezuser.password'],
                 'second_options' => ['label' => 'content.field_type.ezuser.password_confirm'],
             ])
@@ -43,7 +43,7 @@ class UserType extends AbstractType
     {
         $resolver
             ->setDefaults([
-                'data_class' => '\eZ\Publish\Core\FieldType\User\Value',
+                'data_class' => '\EzSystems\RepositoryForms\Data\User\UserAccountFieldData',
                 'translation_domain' => 'ezrepoforms_content',
             ]);
     }
