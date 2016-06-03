77ef74f665a23c1431d9c974f145080600b2b9a6
Bertrand Dunogier <bertrand.dunogier@ez.no>
2016-06-03 18:52:56 +0200

    fixup! Limited User form processor to user data

--- lib/Security/UserRegisterPolicyProvider.php
+++ lib/Security/UserRegisterPolicyProvider.php
@@ -1,24 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bdunogier
- * Date: 03/06/2016
- * Time: 18:03
+ * This file is part of the eZ RepositoryForms package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
  */
-
 namespace EzSystems\RepositoryForms\Security;
 
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigBuilderInterface;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Security\PolicyProvider\PolicyProviderInterface;
 
+/**
+ * Adds the user/register policy.
+ */
 class UserRegisterPolicyProvider implements PolicyProviderInterface
 {
     public function addPolicies(ConfigBuilderInterface $configBuilder)
     {
-        $configBuilder->addConfig([
-            "user" => [
-                "register" => null,
-            ],
-        ]);
+        $configBuilder->addConfig(['user' => ['register' => null]]);
     }
 }
