aeae30416e8e0c67fa0b155529e99d2c6da8ac03
Damien Pobel <dp@ez.no>
2016-11-28 17:11:32 +0100

    EZP-25002: Reorganize the selection field def options markup

--- lib/FieldType/Mapper/SelectionFormMapper.php
+++ lib/FieldType/Mapper/SelectionFormMapper.php
@@ -47,7 +47,7 @@ class SelectionFormMapper implements FieldDefinitionFormMapperInterface, FieldVa
                 'allow_delete' => true,
                 'delete_empty' => true,
                 'prototype' => true,
-                'prototype_name' => count($data->fieldDefinition->fieldSettings['options']),
+                'prototype_name' => '__number__',
                 'required' => false,
                 'property_path' => 'fieldSettings[options]',
                 'label' => 'field_definition.ezselection.options',
