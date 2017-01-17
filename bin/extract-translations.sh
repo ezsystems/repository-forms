0d8c9637357e1cd70f83d915779c6be409aa002a
Nicolas Bastien <nicolas.bastien@ez.no>
2017-01-17 11:02:55 +0100

    EZP-26835 : Script to extract translations + update translation files with it

---
+++ bin/extract-translations.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+echo 'Translation extraction';
+cd ../../..;
+# Extract string for default locale
+echo '# Extract RepositoryForms';
+./app/console translation:extract en -v \
+  --dir=./vendor/ezsystems/repository-forms/bundle \
+  --dir=./vendor/ezsystems/repository-forms/lib \
+  --exclude-dir=vendor \
+  --output-dir=./vendor/ezsystems/repository-forms/bundle/Resources/translations \
+  --enable-extractor=ez_location_sorting \
+  --keep
+  "$@"
+
+echo '# Clean file references';
+sed -i "s|/../../../../.././vendor/ezsystems/repository-forms/|/|g" ./vendor/ezsystems/repository-forms/bundle/Resources/translations/*.xlf
+
+echo 'Translation extraction done';
