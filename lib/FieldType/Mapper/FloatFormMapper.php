7338b8be5914b1e64b189477f977ba4436d19ba2
Adam Wójs <adam@wojs.pl>
2017-04-29 21:38:52 +0200

    EZP-26878: Add "Default value" input field for Checkbox, Float, ISBN, and Integer content definitions in EDIT mode

--- lib/FieldType/Mapper/FloatFormMapper.php
+++ lib/FieldType/Mapper/FloatFormMapper.php
@@ -8,7 +8,9 @@
  */
 namespace EzSystems\RepositoryForms\FieldType\Mapper;
 
+use eZ\Publish\API\Repository\FieldTypeService;
 use EzSystems\RepositoryForms\Data\FieldDefinitionData;
+use EzSystems\RepositoryForms\FieldType\DataTransformer\FieldValueTransformer;
 use EzSystems\RepositoryForms\FieldType\FieldDefinitionFormMapperInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormInterface;
@@ -16,8 +18,30 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FloatFormMapper implements FieldDefinitionFormMapperInterface
 {
-    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data)
+    /**
+     * @var \eZ\Publish\API\Repository\FieldTypeService
+     */
+    private $fieldTypeService;
+
+    public function __construct(FieldTypeService $fieldTypeService)
     {
+        $this->fieldTypeService = $fieldTypeService;
+    }
+
+    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $fieldDefinition)
+    {
+        $defaultValueForm = $fieldDefinitionForm
+            ->getConfig()
+            ->getFormFactory()
+            ->createBuilder()
+            ->create('defaultValue', NumberType::class, [
+                'required' => false,
+                'label' => 'field_definition.ezfloat.default_value',
+            ])
+            ->addModelTransformer(new FieldValueTransformer($this->fieldTypeService->getFieldType($fieldDefinition->getFieldTypeIdentifier())))
+            ->setAutoInitialize(false)
+            ->getForm();
+
         $fieldDefinitionForm
             ->add(
                 'minValue', NumberType::class, [
@@ -32,7 +56,8 @@ class FloatFormMapper implements FieldDefinitionFormMapperInterface
                     'property_path' => 'validatorConfiguration[FloatValueValidator][maxFloatValue]',
                     'label' => 'field_definition.ezfloat.max_value',
                 ]
-            );
+            )
+            ->add($defaultValueForm);
     }
 
     /**
