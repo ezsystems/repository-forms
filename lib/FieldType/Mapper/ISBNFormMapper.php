cc94cb64fa58836e380aa4a2446bf0af6038ab01
Maciej Kobus <maciej@mkobus.com>
2017-07-07 13:23:20 +0200

    EZP-27661: Implement editing support for ISBN FieldType

--- lib/FieldType/Mapper/ISBNFormMapper.php
+++ lib/FieldType/Mapper/ISBNFormMapper.php
@@ -9,15 +9,17 @@
 namespace EzSystems\RepositoryForms\FieldType\Mapper;
 
 use eZ\Publish\API\Repository\FieldTypeService;
+use EzSystems\RepositoryForms\Data\Content\FieldData;
 use EzSystems\RepositoryForms\Data\FieldDefinitionData;
 use EzSystems\RepositoryForms\FieldType\DataTransformer\FieldValueTransformer;
 use EzSystems\RepositoryForms\FieldType\FieldDefinitionFormMapperInterface;
+use EzSystems\RepositoryForms\FieldType\FieldValueFormMapperInterface;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ISBNFormMapper implements FieldDefinitionFormMapperInterface
+class ISBNFormMapper implements FieldDefinitionFormMapperInterface, FieldValueFormMapperInterface
 {
     /**
      * @var \eZ\Publish\API\Repository\FieldTypeService
@@ -52,6 +54,29 @@ class ISBNFormMapper implements FieldDefinitionFormMapperInterface
                 ]
             )
             ->add($defaultValueForm);
+    }
+
+    public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
+    {
+        $fieldDefinition = $data->fieldDefinition;
+        $formConfig = $fieldForm->getConfig();
+
+        $fieldForm
+            ->add(
+                $formConfig->getFormFactory()->createBuilder()
+                    ->create(
+                        'value',
+                        TextType::class,
+                        [
+                            'required' => $fieldDefinition->isRequired,
+                            'label' => $fieldDefinition->getName($formConfig->getOption('languageCode')),
+                        ]
+                    )
+                    ->addModelTransformer(new FieldValueTransformer($this->fieldTypeService->getFieldType($fieldDefinition->fieldTypeIdentifier)))
+                    // Deactivate auto-initialize as we're not on the root form.
+                    ->setAutoInitialize(false)
+                    ->getForm()
+            );
     }
 
     /**
