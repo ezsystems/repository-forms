c2d6fe520a241074d812ec7b0f67fa0525486852
Maciej Kobus <maciej@mkobus.com>
2017-08-04 15:54:51 +0200

    EZP-27628: Implement editing support for RichText FieldType

--- doc/bc/changes-1.10.md
+++ doc/bc/changes-1.10.md
@@ -3,6 +3,12 @@
 Changes affecting version compatibility with former or future versions.
 
 ## Changes
+- EZP-27628: Implement editing support for RichText FieldType
+
+  What changed: `ezrepoforms.field_type.form_mapper.ezrichtext` requires `ezpublish.api.service.field_type` and `ezpublish.fieldType.ezrichtext.converter.edit.xhtml5` dependencies.
+
+  How it might affect your code: If you are implementing a subclass, make sure you are injecting required services.
+  
 - EZP-27641: Implement editing support for Relation FieldType
 
   What changed: Removed `ezpublish.translation_helper` dependency from `ezrepoforms.field_type.form_mapper.abstractrelation` service.
--- lib/FieldType/Mapper/RichTextFormMapper.php
+++ lib/FieldType/Mapper/RichTextFormMapper.php
@@ -8,15 +8,49 @@
  */
 namespace EzSystems\RepositoryForms\FieldType\Mapper;
 
-use EzSystems\RepositoryForms\Data\FieldDefinitionData;
-use EzSystems\RepositoryForms\FieldType\FieldDefinitionFormMapperInterface;
+use eZ\Publish\API\Repository\FieldTypeService;
+use eZ\Publish\Core\FieldType\RichText\Converter;
+use EzSystems\RepositoryForms\Data\Content\FieldData;
+use EzSystems\RepositoryForms\FieldType\DataTransformer\RichTextValueTransformer;
+use EzSystems\RepositoryForms\FieldType\FieldValueFormMapperInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RichTextFormMapper implements FieldDefinitionFormMapperInterface
+class RichTextFormMapper implements FieldValueFormMapperInterface
 {
-    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data)
+    /**
+     * @var FieldTypeService
+     */
+    private $fieldTypeService;
+
+    /**
+     * @var \eZ\Publish\Core\FieldType\RichText\Converter
+     */
+    private $docbookToXhtml5EditConverter;
+
+    public function __construct(FieldTypeService $fieldTypeService, Converter $docbookToXhtml5EditConverter)
     {
+        $this->fieldTypeService = $fieldTypeService;
+        $this->docbookToXhtml5EditConverter = $docbookToXhtml5EditConverter;
+    }
+
+    public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
+    {
+        $fieldDefinition = $data->fieldDefinition;
+        $formConfig = $fieldForm->getConfig();
+
+        $fieldForm
+            ->add(
+                $formConfig->getFormFactory()->createBuilder()
+                    ->create('value', TextareaType::class, [
+                        'required' => $fieldDefinition->isRequired,
+                        'label' => $fieldDefinition->getName($formConfig->getOption('languageCode')),
+                    ])
+                    ->addModelTransformer(new RichTextValueTransformer($this->fieldTypeService->getFieldType($fieldDefinition->fieldTypeIdentifier), $this->docbookToXhtml5EditConverter))
+                    ->setAutoInitialize(false)
+                    ->getForm()
+            );
     }
 
     /**
