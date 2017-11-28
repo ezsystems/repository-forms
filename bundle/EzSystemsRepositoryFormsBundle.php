c108b3594eb220c0a2d78a00641bd3d0d92aab92
Maciej Kobus <maciej.kobus@ez.no>
2017-11-28 15:55:01 +0100

    EZP-27646: Adds user create/edit template configuration parameters

--- bundle/EzSystemsRepositoryFormsBundle.php
+++ bundle/EzSystemsRepositoryFormsBundle.php
@@ -14,9 +14,10 @@ use EzSystems\RepositoryForms\Security\UserRegisterPolicyProvider;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Compiler\FieldTypeFormMapperDispatcherPass;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Compiler\FieldTypeFormMapperPass;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Compiler\LimitationFormMapperPass;
-use EzSystems\RepositoryFormsBundle\DependencyInjection\Configuration\Parser\ContentEdit;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Compiler\LimitationValueMapperPass;
+use EzSystems\RepositoryFormsBundle\DependencyInjection\Configuration\Parser\ContentEdit;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Configuration\Parser\LimitationValueTemplates;
+use EzSystems\RepositoryFormsBundle\DependencyInjection\Configuration\Parser\UserEdit;
 use EzSystems\RepositoryFormsBundle\DependencyInjection\Configuration\Parser\UserRegistration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -35,6 +36,7 @@ class EzSystemsRepositoryFormsBundle extends Bundle
         $eZExtension->addPolicyProvider(new UserRegisterPolicyProvider());
         $eZExtension->addConfigParser(new UserRegistration());
         $eZExtension->addConfigParser(new ContentEdit());
+        $eZExtension->addConfigParser(new UserEdit());
         $eZExtension->addConfigParser(new LimitationValueTemplates());
         $eZExtension->addDefaultSettings(__DIR__ . '/Resources/config', ['ezpublish_default_settings.yml']);
     }
