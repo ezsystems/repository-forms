6180ee7f40d466e3142b244922d073f9329dd5ba
André R <andre.romcke@gmail.com>
2017-12-22 10:28:22 +0100

    [Travis] Use Trusty (#194)
    * [Travis] Use Trusty
    
    * [Composer] Set tmp_ci_branch alias
    
    * [Travis] Update to use trusty scritp
    
    * [Travis] Test all stable branches
    
    [skip ci]
    
    * [Travis] Set compose file env
    
    * [Travis] Set symfony env
    
    * [Travis] Run behat via docker
    
    * [Travis] Set php image versions

--- bin/.travis/prepare_behat.sh
+++ bin/.travis/prepare_behat.sh
@@ -3,9 +3,8 @@
 # File for setting up system for behat testing, just like done in DemoBundle's .travis.yml
 
 # Change local git repo to be a full one as we will reuse it for composer install below
-git fetch --unshallow && git checkout -b tmp_travis_branch
-export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR
-export TRAVIS_BUILD_DIR="$HOME/build/ezplatform"
+git fetch --unshallow && git checkout -b tmp_ci_branch
+export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR TRAVIS_BUILD_DIR="$HOME/build/ezplatform"
 
 # Checkout meta repo, use the branch indicated in composer.json under extra._ezplatform_branch_for_behat_tests
 EZPLATFORM_BRANCH=`php -r 'echo json_decode(file_get_contents("./composer.json"))->extra->_ezplatform_branch_for_behat_tests;'`
@@ -15,5 +14,7 @@ cd "$HOME/build"
 git clone --depth 1 --single-branch --branch "$EZPLATFORM_BRANCH" https://github.com/ezsystems/ezplatform.git
 cd ezplatform
 
+export RUN_INSTALL=1
+
 # Install everything needed for behat testing, using our local branch of this repo
-./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/repository-forms:dev-tmp_travis_branch as 1.4"
+./bin/.travis/trusty/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/repository-forms:dev-tmp_ci_branch"
