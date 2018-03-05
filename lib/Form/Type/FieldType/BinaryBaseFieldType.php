ec8da2941d9ad10b0f211b31e5a3a71d825621b1
Adam Wójs <adam.wojs@ez.no>
2018-03-05 12:51:01 +0100

    EZP-28871: upload_max_filesize setting should be taken into account in the validation of uploaded files

--- lib/Form/Type/FieldType/BinaryBaseFieldType.php
+++ lib/Form/Type/FieldType/BinaryBaseFieldType.php
@@ -9,7 +9,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Parent Form Type for binary file based field types.
@@ -32,12 +35,50 @@ class BinaryBaseFieldType extends AbstractType
                 [
                     'label' => /** @Desc("File") */ 'content.field_type.binary_base.file',
                     'required' => $options['required'],
+                    'constraints' => [
+                        new Assert\File([
+                            'maxSize' => $this->getMaxUploadSize()
+                        ])
+                    ]
                 ]
             );
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['max_upload_size'] = $this->getMaxUploadSize();
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(['translation_domain' => 'ezrepoforms_fieldtype']);
+    }
+
+    private function getMaxUploadSize()
+    {
+        static $value = null;
+        if ($value === null) {
+            return $this->str2bytes(ini_get('upload_max_filesize'));
+        }
+
+        return $value;
+    }
+
+    private function str2bytes($str)
+    {
+        $str = strtoupper(trim($str));
+
+        $value = substr($str, 0, -1);
+        $unit  = substr($str, -1);
+        switch($unit) {
+            case 'G':
+                $value *= 1024;
+            case 'M':
+                $value *= 1024;
+            case 'K':
+                $value *= 1024;
+        }
+
+        return (int) $value;
     }
 }
