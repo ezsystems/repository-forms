bfed6a7a0d48cfbc79dce39fd0301f37c701bfb1
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-04-25 14:10:22 +0200

    EZP-29024: Adding policy user/register to the role throws 500

--- lib/Security/UserRegisterPolicyProvider.php
+++ lib/Security/UserRegisterPolicyProvider.php
@@ -11,12 +11,14 @@ use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigBuilde
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Security\PolicyProvider\PolicyProviderInterface;
 
 /**
+ * @deprecated Deprecated since 2.1. No longer used. This policy was moved to the eZ/Publish/Core/settings/policies.yml.
+ *
  * Adds the user/register policy.
  */
 class UserRegisterPolicyProvider implements PolicyProviderInterface
 {
     public function addPolicies(ConfigBuilderInterface $configBuilder)
     {
-        $configBuilder->addConfig(['user' => ['register' => null]]);
+        @trigger_error('Method ' . __METHOD__ . ' is deprecated since 2.1', E_USER_DEPRECATED);
     }
 }
