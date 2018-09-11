22d9d75fa130868536181261c8142e2928fc676e
Adam Wójs <adam@wojs.pl>
2018-09-11 20:53:34 +0200

    EZP-29104: Implemented ImageAsset Field Type (#246)
    * EZP-29104: Implemented ImageAsset Field Type
    
    Co-authored-by: Dawid Parafiński <dawid.parafinski@ez.no>

---
+++ lib/FieldType/DataTransformer/ImageAssetValueTransformer.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\RepositoryForms\FieldType\DataTransformer;
+
+use eZ\Publish\Core\FieldType\ImageAsset\Value;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class ImageAssetValueTransformer extends AbstractBinaryBaseTransformer implements DataTransformerInterface
+{
+    /**
+     * @param \eZ\Publish\Core\FieldType\ImageAsset\Value|null $value
+     *
+     * @throws \Symfony\Component\Form\Exception\TransformationFailedException
+     *
+     * @return array|null
+     */
+    public function transform($value): ?array
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        if (!$value instanceof Value) {
+            throw new TransformationFailedException(
+                sprintf('Expected a %s, got %s instead', Value::class, gettype($value))
+            );
+        }
+
+        return array_merge(
+            $this->getDefaultProperties(),
+            ['destinationContentId' => $value->destinationContentId]
+        );
+    }
+
+    /**
+     * @param array|null $value
+     *
+     * @throws \Symfony\Component\Form\Exception\TransformationFailedException
+     *
+     * @return \eZ\Publish\Core\FieldType\ImageAsset\Value|null
+     */
+    public function reverseTransform($value): ?Value
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        if (!is_array($value)) {
+            throw new TransformationFailedException(
+                sprintf('Expected a array, got %s instead', gettype($value))
+            );
+        }
+
+        return new Value($value['destinationContentId']);
+    }
+}
