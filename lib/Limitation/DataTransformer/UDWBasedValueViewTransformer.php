408dded9934fd2f1bb00b6f51be37ec00f0051ac
Adam Wójs <adam@wojs.pl>
2019-01-14 20:35:11 +0100

    EZP-29849: Allow user to remove location in subtree/location limitation

---
+++ lib/Limitation/DataTransformer/UDWBasedValueViewTransformer.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\RepositoryForms\Limitation\DataTransformer;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Exceptions\UnauthorizedException;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Values\Content\Location;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class UDWBasedValueViewTransformer implements DataTransformerInterface
+{
+    const DELIMITER = ',';
+
+    /** @var \eZ\Publish\API\Repository\LocationService */
+    private $locationService;
+
+    /**
+     * @param \eZ\Publish\API\Repository\LocationService $locationService
+     */
+    public function __construct(LocationService $locationService)
+    {
+        $this->locationService = $locationService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value)
+    {
+        if (!is_array($value)) {
+            return null;
+        }
+
+        return implode(self::DELIMITER, array_map(function (Location $location) {
+            return $location->id;
+        }, $value));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($value)
+    {
+        if (!is_string($value) || $value === '') {
+            return $value;
+        }
+
+        try {
+            return array_map(function ($id) {
+                return $this->locationService->loadLocation($id);
+            }, explode(self::DELIMITER, $value));
+        } catch (NotFoundException | UnauthorizedException $e) {
+            throw new TransformationFailedException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
