673a419ad8fa42fd9ce9451151a655e840556de3
Pawel Bujakowski <pawelbujakowski@gmail.com>
2019-05-30 09:14:55 +0200

    revert pagelayout variable

--- lib/EventListener/ViewTemplatesListener.php
+++ lib/EventListener/ViewTemplatesListener.php
@@ -64,7 +64,7 @@ class ViewTemplatesListener implements EventSubscriberInterface
         foreach ($this->viewTemplates as $viewClass => $template) {
             if ($view instanceof $viewClass) {
                 $view->setTemplateIdentifier($template);
-                $view->addParameters(['pageLayout' => $this->pagelayout]);
+                $view->addParameters(['pagelayout' => $this->pagelayout]);
             }
         }
     }
