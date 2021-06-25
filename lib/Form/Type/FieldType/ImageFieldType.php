56b80703a1f2189b3175128eef017102e2c3c659
Adam Wójs <adam@wojs.pl>
2021-06-25 10:38:52 +0200

    IBX-154: Allowed to define if the alternative text for image field is required (2.5) (#348)

--- lib/Form/Type/FieldType/ImageFieldType.php
+++ lib/Form/Type/FieldType/ImageFieldType.php
@@ -8,6 +8,8 @@ namespace EzSystems\RepositoryForms\Form\Type\FieldType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -38,13 +40,25 @@ class ImageFieldType extends AbstractType
                 TextType::class,
                 [
                     'label' => /** @Desc("Alternative text") */ 'content.field_type.ezimage.alternative_text',
-                    'required' => false,
+                    'required' => $options['is_alternative_text_required'],
                 ]
             );
     }
 
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars += [
+            'is_alternative_text_required' => $options['is_alternative_text_required'],
+        ];
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(['translation_domain' => 'ezrepoforms_fieldtype']);
+        $resolver->setDefaults([
+            'translation_domain' => 'ezrepoforms_fieldtype',
+            'is_alternative_text_required' => false,
+        ]);
+
+        $resolver->setAllowedTypes('is_alternative_text_required', 'bool');
     }
 }
